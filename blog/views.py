f2f2975f877be4c12cedfe2c1a9666354c22fc82
xnvtvx <khmurova.nataliia@gmail.com>
2017-06-25 16:24:41 +0200

    apply security changes

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -15,6 +14,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -45,6 +45,7 @@ def publish(self):
     self.published_date = timezone.now()
     self.save()
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
